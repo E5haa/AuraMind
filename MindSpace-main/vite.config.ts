8e988ffce51dcb8f663de2be0a0f57ab4bf00cfb
E5shaa <hellons0811@gmail.com>
2025-09-20 15:48:57 +0530

    new

--- MindSpace-main/vite.config.ts
+++ MindSpace-main/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "c4pxqj-8080.csb.app",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
